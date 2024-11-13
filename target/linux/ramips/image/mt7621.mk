e1bb80cd3e5f2e267d0d41beca6530ace3ff1f1b
azx123123 <502048529@qq.com>
2024-11-13 12:22:31 +0000

    修改k2p大小配置

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -1972,7 +1972,7 @@ TARGET_DEVICES += oraybox_x3a
 
 define Device/phicomm_k2p
   $(Device/dsa-migration)
-  IMAGE_SIZE := 15744k
+  IMAGE_SIZE := 31744k
   DEVICE_VENDOR := Phicomm
   DEVICE_MODEL := K2P
   DEVICE_ALT0_VENDOR := Phicomm
